6250187dad37af17799a108300168878d4393693
atashnezhad <atashnezhad1@gmail.com>
2023-05-09 23:04:09 -0500

    some works

--- download_images.py
+++ download_images.py
@@ -5,5 +5,5 @@ from bing_image_downloader import downloader
 downloader.download("pdc_bit", limit=200,  output_dir='dataset',
                     adult_filter_off=True, force_replace=False, timeout=120)
 
-# downloader.download("rollercone_bit", limit=200, output_dir='dataset',
-#                     adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("rollercone_bit", limit=200, output_dir='dataset',
+                    adult_filter_off=True, force_replace=False, timeout=60)
--- neural_network_model/P02.py
+++ neural_network_model/P02.py
@@ -4,7 +4,8 @@ import os
 # import sys
 import warnings
 from pathlib import Path
-from typing import List
+from random import random
+from typing import List, Any, Dict
 
 warnings.filterwarnings('ignore')
 # from collections import Counter
@@ -15,12 +16,12 @@ warnings.filterwarnings('ignore')
 # # import tensorflow as tf
 # from tensorflow import keras
 # import tensorflow
-#
-# from tensorflow.keras.callbacks import ModelCheckpoint
-# from tensorflow.keras.layers import *
-# from tensorflow.keras.models import *
-# from tensorflow.keras.preprocessing import image
-# from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import *
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 
 
 class BitVision:
@@ -28,38 +29,83 @@ class BitVision:
     This class is used to train the neural network model.
     for the bit type detection.
     """
-    def __init_(self, *args, **kwargs):
-        pass
+    def __init__(self, *args, **kwargs):
 
-    def read_data(self, *, folders_name: List[str] = None,
-                  print_file_names: bool = False) -> None:
+        self.dataset_address = kwargs.get("dataset_address", None)
+
+        self.image_dict: Dict[str, Dict[str, Any]] = {}
+
+        self.categories_name_folders = None
+        self.find_categories_name()
+
+    def find_categories_name(self) -> None:
+        """
+        This function is used to find the categories name.
+        :return:
+        """
+        self.categories_name_folders = os.listdir(self.dataset_address)
+        list_to_ignore = [".DS_Store"]
+        self.categories_name_folders = [x for x in self.categories_name_folders if x not in list_to_ignore]
+        # print(self.categories_name_folders)
+
+    def read_data(self, *, print_file_names: bool = False) -> None:
         """
         This function prints the number of files in the dataset folder.
         :param print_file_names:
         :param folders_name: list of folders name
         :return:
         """
-        my_dict = {}
 
-        for folder_name in folders_name:
+        for category_folder in self.categories_name_folders:
             # read the files in the dataset folder
             main_dataset_folder = Path(__file__).parent / ".." / "dataset"
-            data_address = main_dataset_folder / folder_name
+            data_address = main_dataset_folder / category_folder
 
             if print_file_names:
                 print("List of files in the folder:")
                 for file in data_address.iterdir():
                     print(file.name)
 
-            # print number of files in the folder
-            # print(f"Number of files in the folder {folder_name}:")
-            # print(len(list(data_address.iterdir())))
+            self.image_dict[category_folder]["image_list"] = list(data_address.iterdir())
+            self.image_dict[category_folder]["number_of_images"] = len(list(data_address.iterdir()))
 
-            my_dict[folder_name] = len(list(data_address.iterdir()))
+        print(self.image_dict)
 
-        print(my_dict)
+    def balance_data(self, number_of_images_tobe_gen: int = 200):
+        """
+        This function is used to balance the data.
+        :return:
+        """
+        Augment_data_gen = image.ImageDataGenerator(
+            rotation_range=45,
+            width_shift_range=0.0,
+            height_shift_range=0.0,
+            shear_range=0.2,
+            zoom_range=0.5,
+            horizontal_flip=True,
+            fill_mode='nearest')
+
+        for image_category in self.image_dict.keys():
+            for i in range(0, number_of_images_tobe_gen):
+
+                # select a random img
+                rand_img_num = random.randint(0, NORMAL_count - 1)
+                img_name = NORMAL_img_list[rand_img_num]
+
+                img_address = NORMAL_dir + img_name
+                img = load_img(img_address)
+
+                x = img_to_array(img)
+                x = x.reshape((1,) + x.shape)
+
+                for batch in Augment_data_gen. \
+                        flow(x, batch_size=1,
+                             save_to_dir='../Dataset_augmented/NORMAL/',
+                             save_prefix='augmented_normal',
+                             save_format='jpeg'):
+                    break
 
 
 if __name__ == "__main__":
-    obj = BitVision()
-    obj.read_data(folders_name=["pdc_bit", "rollercone_bit"], print_file_names=False)
+    obj = BitVision(dataset_address="../dataset")
+    obj.read_data(print_file_names=False)
