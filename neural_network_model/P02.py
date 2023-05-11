e2101e60a5e599de06701c3d56653b18ec2c6312
atashnezhad <atashnezhad1@gmail.com>
2023-05-10 22:57:54 -0500

    train test split done

--- neural_network_model/P02.py
+++ neural_network_model/P02.py
@@ -1,20 +1,26 @@
 # import pandas as pd
-import os
-from model import AUGMENTATION_SETTING
 import logging
+import os
+import random
+import shutil
 
-# import shutil
-from tqdm import tqdm
+from model import SETTING
+from bing_image_downloader import downloader
+
+# set seed to get the same random numbers each time
+random.seed(1)
+import random
 
 # import sys
 import warnings
 from pathlib import Path
-from random import random
-from typing import List, Any, Dict
+from typing import Any, Dict, List
+
+# import shutil
+from tqdm import tqdm
 
 warnings.filterwarnings("ignore")
 # from collections import Counter
-# import random
 #
 # import numpy as np
 # import matplotlib.pyplot as plt
@@ -28,8 +34,8 @@ from tensorflow.keras.models import *
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import (
     ImageDataGenerator,
-    load_img,
     img_to_array,
+    load_img,
 )
 
 # Initialize the logger
@@ -58,6 +64,20 @@ class BitVision:
         self.image_dict: Dict[str, Dict[str, Any]] = {}
         self.categories_name_folders = None
         self.find_categories_name()
+
+    @staticmethod
+    def download_images(category_list=None) -> None:
+        if category_list is None:
+            category_list = SETTING.CATEGORY_SETTING.CATEGORIES
+        for category in category_list:
+            downloader.download(
+                category,
+                limit=200,
+                output_dir=Path(__file__).parent / ".." / "dataset",
+                adult_filter_off=True,
+                force_replace=False,
+                timeout=120,
+            )
 
     def find_categories_name(self) -> None:
         """
@@ -117,28 +137,31 @@ class BitVision:
         :return:
         """
         Augment_data_gen = image.ImageDataGenerator(
-            rotation_range=AUGMENTATION_SETTING.ROTATION_RANGE,
-            width_shift_range=AUGMENTATION_SETTING.WIDTH_SHIFT_RANGE,
-            height_shift_range=AUGMENTATION_SETTING.HEIGHT_SHIFT_RANGE,
-            shear_range=AUGMENTATION_SETTING.SHEAR_RANGE,
-            zoom_range=AUGMENTATION_SETTING.ZOOM_RANGE,
-            horizontal_flip=AUGMENTATION_SETTING.HORIZONTAL_FLIP,
-            fill_mode=AUGMENTATION_SETTING.FILL_MODE,
+            rotation_range=SETTING.AUGMENTATION_SETTING.ROTATION_RANGE,
+            width_shift_range=SETTING.AUGMENTATION_SETTING.WIDTH_SHIFT_RANGE,
+            height_shift_range=SETTING.AUGMENTATION_SETTING.HEIGHT_SHIFT_RANGE,
+            shear_range=SETTING.AUGMENTATION_SETTING.SHEAR_RANGE,
+            zoom_range=SETTING.AUGMENTATION_SETTING.ZOOM_RANGE,
+            horizontal_flip=SETTING.AUGMENTATION_SETTING.HORIZONTAL_FLIP,
+            fill_mode=SETTING.AUGMENTATION_SETTING.FILL_MODE,
         )
 
         for image_category in self.image_dict.keys():
             # check if a dir dataset_augmented exists if not create it
             if not os.path.exists(
-                AUGMENTATION_SETTING.AUGMENTED_IMAGES_DIR_ADDRESS / image_category
+                SETTING.AUGMENTATION_SETTING.AUGMENTED_IMAGES_DIR_ADDRESS
+                / image_category
             ):
                 os.makedirs(
-                    AUGMENTATION_SETTING.AUGMENTED_IMAGES_DIR_ADDRESS / image_category
+                    SETTING.AUGMENTATION_SETTING.AUGMENTED_IMAGES_DIR_ADDRESS
+                    / image_category
                 )
             # check if the dir is empty if not delete all the files
             # TODO: check if this is needed or not
             else:
                 for file in (
-                    AUGMENTATION_SETTING.AUGMENTED_IMAGES_DIR_ADDRESS / image_category
+                    SETTING.AUGMENTATION_SETTING.AUGMENTED_IMAGES_DIR_ADDRESS
+                    / image_category
                 ).iterdir():
                     os.remove(file)
 
@@ -148,7 +171,7 @@ class BitVision:
                 desc=f"Augmenting {image_category} images:",
             ):
                 # generate a random number integer between 0 and number_of_images
-                rand_img_num = int(random() * number_of_images)
+                rand_img_num = int(random.random() * number_of_images)
 
                 img_address = self.image_dict[image_category]["image_list"][
                     rand_img_num
@@ -165,11 +188,11 @@ class BitVision:
 
                 for _ in Augment_data_gen.flow(
                     x,
-                    batch_size=AUGMENTATION_SETTING.BATCH_SIZE,
-                    save_to_dir=AUGMENTATION_SETTING.AUGMENTED_IMAGES_DIR_ADDRESS
+                    batch_size=SETTING.AUGMENTATION_SETTING.BATCH_SIZE,
+                    save_to_dir=SETTING.AUGMENTATION_SETTING.AUGMENTED_IMAGES_DIR_ADDRESS
                     / image_category,
-                    save_prefix=AUGMENTATION_SETTING.AUGMENTED_IMAGES_SAVE_PREFIX,
-                    save_format=AUGMENTATION_SETTING.AUGMENTED_IMAGES_SAVE_FORMAT,
+                    save_prefix=SETTING.AUGMENTATION_SETTING.AUGMENTED_IMAGES_SAVE_PREFIX,
+                    save_format=SETTING.AUGMENTATION_SETTING.AUGMENTED_IMAGES_SAVE_FORMAT,
                 ):
                     break
 
@@ -178,62 +201,94 @@ class BitVision:
         This function is used to split the data into train and test.
         :return:
         """
-        train_fraction = kwargs.get("train_fraction", 0.7)
-        test_fraction = kwargs.get("test_fraction", 0.2)
-        validation_fraction = kwargs.get("validation_fraction", 0.1)
-
-        logger.info(f"Train fraction: {train_fraction}")
-        logger.info(f"Test fraction: {test_fraction}")
-        logger.info(f"Validation fraction: {validation_fraction}")
-
         # get the list of dirs in the AUGMENTED_IMAGES_DIR_ADDRESS
         augmented_images_dir_list = os.listdir(
-            AUGMENTATION_SETTING.AUGMENTED_IMAGES_DIR_ADDRESS
+            SETTING.AUGMENTATION_SETTING.AUGMENTED_IMAGES_DIR_ADDRESS
         )
         logger.info(f"Augmented images dir list: {augmented_images_dir_list}")
 
         # make a new dir for train and test and validation data
-        if not os.path.exists(AUGMENTATION_SETTING.TRAIN_TEST_SPLIT_DIR_ADDRESS):
-            os.makedirs(AUGMENTATION_SETTING.TRAIN_TEST_SPLIT_DIR_ADDRESS)
+        if not os.path.exists(
+            SETTING.PREPROCESSING_SETTING.TRAIN_TEST_SPLIT_DIR_ADDRESS
+        ):
+            os.makedirs(SETTING.PREPROCESSING_SETTING.TRAIN_TEST_SPLIT_DIR_ADDRESS)
         # make 3 dirs for train, test and validation under AUGMENTATION_SETTING.TRAIN_TEST_SPLIT_DIR_ADDRESS
-        for dir_name in AUGMENTATION_SETTING.TRAIN_TEST_SPLIT_DIR_NAMES:
+        for dir_name in SETTING.PREPROCESSING_SETTING.TRAIN_TEST_SPLIT_DIR_NAMES:
             if not os.path.exists(
-                AUGMENTATION_SETTING.TRAIN_TEST_SPLIT_DIR_ADDRESS / dir_name
+                SETTING.PREPROCESSING_SETTING.TRAIN_TEST_SPLIT_DIR_ADDRESS / dir_name
             ):
                 os.makedirs(
-                    AUGMENTATION_SETTING.TRAIN_TEST_SPLIT_DIR_ADDRESS / dir_name
+                    SETTING.PREPROCESSING_SETTING.TRAIN_TEST_SPLIT_DIR_ADDRESS
+                    / dir_name
                 )
         logger.info(f"Created train, test and validation dirs")
 
         # under each train, test and validation dir make a dir for each category
-        for dir_name in AUGMENTATION_SETTING.TRAIN_TEST_SPLIT_DIR_NAMES:
+        for dir_name in SETTING.PREPROCESSING_SETTING.TRAIN_TEST_SPLIT_DIR_NAMES:
             for category in augmented_images_dir_list:
                 if not os.path.exists(
-                    AUGMENTATION_SETTING.TRAIN_TEST_SPLIT_DIR_ADDRESS
+                    SETTING.PREPROCESSING_SETTING.TRAIN_TEST_SPLIT_DIR_ADDRESS
                     / dir_name
                     / category
                 ):
                     os.makedirs(
-                        AUGMENTATION_SETTING.TRAIN_TEST_SPLIT_DIR_ADDRESS
+                        SETTING.PREPROCESSING_SETTING.TRAIN_TEST_SPLIT_DIR_ADDRESS
                         / dir_name
                         / category
                     )
         logger.info(f"Created category dirs under train, test and validation dirs")
 
+        self.populated_augmented_images_into_train_test_val_dirs()
 
+    def populated_augmented_images_into_train_test_val_dirs(self):
+        for category in self.categories_name_folders:
+            # get the list of images in the dataset_augmented pdc_bit folder
+            original_list = os.listdir(
+                SETTING.AUGMENTATION_SETTING.AUGMENTED_IMAGES_DIR_ADDRESS / category
+            )
 
+            # Number of items to select for each new list
+            num_train = int(
+                len(original_list) * SETTING.PREPROCESSING_SETTING.TRAIN_FRACTION
+            )
+            num_test = int(
+                len(original_list) * SETTING.PREPROCESSING_SETTING.TEST_FRACTION
+            )
+            num_val = len(original_list) - num_train - num_test
 
+            # Randomly select items from the original list without replacement
+            selected_items = random.sample(original_list, len(original_list))
 
+            # Create three new lists containing 70%, 20%, and 10% of the original list
+            train_list = selected_items[:num_train]
+            test_list = selected_items[num_train : num_train + num_test]
+            val_list = selected_items[num_train + num_test :]
+            # TODO: check if the dirs are not empty if not delete all the files
+            # copy the images from the dataset_augmented pdc_bit folder to the train, test and validation folders
+            self.copy_images(train_list, category, "train")
+            self.copy_images(test_list, category, "test")
+            self.copy_images(val_list, category, "val")
+
+    @staticmethod
+    def copy_images(images, categ, dest_folder):
+        for image in images:
+            shutil.copy(
+                SETTING.AUGMENTATION_SETTING.AUGMENTED_IMAGES_DIR_ADDRESS
+                / categ
+                / image,
+                SETTING.PREPROCESSING_SETTING.TRAIN_TEST_SPLIT_DIR_ADDRESS
+                / dest_folder
+                / categ
+                / image,
+            )
+        logger.info(
+            f"copied {len(images)} images for category {categ} to {dest_folder}"
+        )
 
 
 if __name__ == "__main__":
     obj = BitVision(dataset_address=Path(__file__).parent / ".." / "dataset")
+    # BitVision.download_images()
     # obj.read_data(print_file_names=False)
     # obj.balance_data(number_of_images_tobe_gen=200)
-
-    train_test_validation_split = {
-        "train_fraction": 0.7,
-        "test_fraction": 0.2,
-        "validation_fraction": 0.1,
-    }
-    obj.train_test_split(**train_test_validation_split)
+    obj.train_test_split()
--- neural_network_model/model.py
+++ neural_network_model/model.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from pydantic import BaseModel
 
 
-class AugmentationSetting(BaseModel):
+class AugmentionSetting(BaseModel):
     ROTATION_RANGE: int = 65
     WIDTH_SHIFT_RANGE: float = 0.0
     HEIGHT_SHIFT_RANGE: float = 0.0
@@ -18,12 +18,28 @@ class AugmentationSetting(BaseModel):
     AUGMENTED_IMAGES_DIR_ADDRESS: str = (
         Path(__file__).parent / ".." / "dataset_augmented"
     )
+    AUGMENTED_IMAGES_SAVE_PREFIX: str = "augmented_image"
+    AUGMENTED_IMAGES_SAVE_FORMAT: str = "jpeg"
+
+
+class CategorySetting(BaseModel):
+    CATEGORIES: list = ["pdc_bit", "rollercone_bit"]
+
+
+class PreprocessingSetting(BaseModel):
     TRAIN_TEST_SPLIT_DIR_ADDRESS: str = (
         Path(__file__).parent / ".." / "dataset_train_test_val"
     )
-    AUGMENTED_IMAGES_SAVE_PREFIX: str = "augmented_image"
-    AUGMENTED_IMAGES_SAVE_FORMAT: str = "jpeg"
     TRAIN_TEST_SPLIT_DIR_NAMES: list = ["train", "test", "val"]
+    TRAIN_FRACTION: float = 0.7
+    TEST_FRACTION: float = 0.2
+    VAL_FRACTION: float = 0.1
 
 
-AUGMENTATION_SETTING = AugmentationSetting()
+class Setting(BaseModel):
+    AUGMENTATION_SETTING: AugmentionSetting = AugmentionSetting()
+    PREPROCESSING_SETTING: PreprocessingSetting = PreprocessingSetting()
+    CATEGORY_SETTING: CategorySetting = CategorySetting()
+
+
+SETTING = Setting()
