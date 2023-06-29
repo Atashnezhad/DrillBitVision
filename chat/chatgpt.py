0bee5b3120cdb747f8967170ea4789ad970b0f08
atashnezhad <atashnezhad1@gmail.com>
2023-06-29 12:52:24 -0500

    fix chat

--- chat/chatgpt.py
+++ chat/chatgpt.py
@@ -1,6 +1,14 @@
+import logging
 import os
+
 import requests
 from dotenv import load_dotenv
+
+# Initialize the logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+import openai
 
 # Load the API key from the .env file
 load_dotenv()
@@ -9,22 +17,28 @@ API_ENDPOINT = os.getenv("CHATGPT_API_ENDPOINT")
 
 
 def chat_with_gpt(messages):
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {API_KEY}"
-    }
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {API_KEY}"}
     payload = {
-        "messages": messages
+        "model": "gpt-3.5-turbo",
+        "messages": messages,
+        "temprature": 1.0,
+        "n": 1,
+        "presence_penalty": 0,
+        "frequency_penalty": 0,
     }
     response = requests.post(API_ENDPOINT, json=payload, headers=headers)
     data = response.json()
-    return data["choices"][0]["message"]["content"]
+    try:
+        if data["choices"][0]["message"]["content"]:
+            return data["choices"][0]["message"]["content"]
+    except:
+        logging.info(f"{data}")
 
 
 def run():
     conversation = [
         {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the World Series in 2020?"}
+        {"role": "user", "content": "Who won the World Series in 2020?"},
     ]
 
     response = chat_with_gpt(conversation)
